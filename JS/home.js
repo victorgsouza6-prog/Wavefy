3f371a1cb7dc5957a9b2b2de1f30cf332c6cb34c
Victor Gabriel De Souza <445616624@ist.senai>
2026-08-06 08:42:36 -0300

    mudei

--- JS/home.js
+++ JS/home.js
@@ -28,112 +28,112 @@ const musicas = [
         nome: "VEIGH",
         artista: "VEIGH",
         capa: "imagens/capas/veigh.png",
-        audio: "musicas/VEIGH.mp3"
+        audio: "Musicas/VEIGH.mp3"
     },
 
     {
         nome: "Pedro Sampaio",
         artista: "Pedro Sampaio",
         capa: "imagens/capas/pedro.sampaio.png",
-        audio: "musicas/PEDRO SAMPAIO.mp3"
+        audio: "Musicas/PEDRO SAMPAIO.mp3"
     },
 
     {
         nome: "MC Livinho",
         artista: "MC Livinho",
         capa: "imagens/capas/livinho.png",
-        audio: "musicas/MC Livinho.mp3"
+        audio: "Musicas/MC Livinho.mp3"
     },
 
     {
         nome: "MC Kevin",
         artista: "MC Kevin",
         capa: "imagens/capas/mc.kevin.png",
-        audio: "musicas/Mc Kevin.mp3"
+        audio: "Musicas/Mc Kevin.mp3"
     },
 
     {
         nome: "MC Jacaré",
         artista: "MC Jacaré",
         capa: "imagens/capas/mc.jacare.png",
-        audio: "musicas/MC Jacaré.mp3"
+        audio: "Musicas/MC Jacaré.mp3"
     },
 
     {
         nome: "Matuê",
         artista: "Matuê",
         capa: "imagens/capas/matue.png",
-        audio: "musicas/Matuê.mp3"
+        audio: "Musicas/Matuê.mp3"
     },
 
     {
         nome: "Marília Mendonça",
         artista: "Marília Mendonça",
         capa: "imagens/capas/marilia.png",
-        audio: "musicas/Marília Mendonça.mp3"
+        audio: "Musicas/Marília Mendonça.mp3"
     },
 
     {
         nome: "Luan Santana",
         artista: "Luan Santana",
         capa: "imagens/capas/luan.santana.png",
-        audio: "musicas/Luan Santana.mp3"
+        audio: "Musicas/Luan Santana.mp3"
     },
 
     {
         nome: "Luan Pereira",
         artista: "Luan Pereira",
         capa: "imagens/capas/luan.pereira.png",
-        audio: "musicas/Luan Pereira.mp3"
+        audio: "Musicas/Luan Pereira.mp3"
     },
 
     {
         nome: "Jorge & Mateus",
         artista: "Jorge & Mateus",
         capa: "imagens/capas/jorge.e.mateus.png",
-        audio: "musicas/Jorge & Mateus.mp3"
+        audio: "Musicas/Jorge & Mateus.mp3"
     },
 
     {
         nome: "João Gomes",
         artista: "João Gomes",
         capa: "imagens/capas/joao.gomes.png",
-        audio: "musicas/João Gomes.mp3"
+        audio: "Musicas/João Gomes.mp3"
     },
 
     {
         nome: "Israel & Rodolffo",
         artista: "Israel & Rodolffo",
         capa: "imagens/capas/israel.png",
-        audio: "musicas/Israel & Rodolffo.mp3"
+        audio: "Musicas/Israel & Rodolffo.mp3"
     },
 
     {
         nome: "Gusttavo Lima",
         artista: "Gusttavo Lima",
         capa: "imagens/capas/gustavo.png",
-        audio: "musicas/gusttavo-limaa.mp3"
+        audio: "Musicas/gusttavo-limaa.mp3"
     },
 
     {
         nome: "Felipe Amorim",
         artista: "Felipe Amorim",
         capa: "imagens/capas/felipe.amorin.png",
-        audio: "musicas/Felipe Amorim.mp3"
+        audio: "Musicas/Felipe Amorim.mp3"
     },
 
     {
         nome: "Anitta",
         artista: "Anitta",
         capa: "imagens/capas/anitta.png",
-        audio: "musicas/Anitta.mp3"
+        audio: "Musicas/Anitta.mp3"
     },
 
     {
         nome: "Ana Castela",
         artista: "Ana Castela",
         capa: "imagens/capas/ana.castela.png",
-        audio: "musicas/Ana Castela.mp3"
+        audio: "Musicas/Ana Castela.mp3"
     }
 
 ];
